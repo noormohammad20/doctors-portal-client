930ac304845392b5d86ee0f7e776764a06393511
noormohammad20 <mdsaim204@gmail.com>
2022-05-10 01:15:08 +0600

    Make Info Card Dynamic with dynamic CSS Class

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,10 +5,12 @@ import marker from '../../assets/icons/marker.svg'
 import phone from '../../assets/icons/phone.svg'
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={marker}></InfoCard>
-            <InfoCard img={phone}></InfoCard>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard cardTitle="Opening Hours" cardText="6pm to 11pm without friday and holydays" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+
+            <InfoCard cardTitle="Visit Our Location" cardText="101, south mughda para dhaka" bgClass="bg-accent" img={marker}></InfoCard>
+
+            <InfoCard cardTitle="Contact Us" cardText="mobile:     +8801719555207" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     )
 }
