7367df8b2c4a69a4f058fa87a8956127d3ac0d9d
noormohammad20 <mdsaim204@gmail.com>
2022-05-09 22:52:35 +0600

    add navbar form daisyui and setrouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import About from './Pages/About/About'
+import Home from './Pages/Home/Home'
+import Navbar from './Pages/Shared/Navbar'
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route pat='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   )
 }
