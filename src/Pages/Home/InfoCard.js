3f65bc8be9eb122112680fd1f5ace5c2390f9b82
noormohammad20 <mdsaim204@gmail.com>
2022-05-10 23:56:16 +0600

    fixed some style issue

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -4,7 +4,7 @@ const InfoCard = ({ img, cardTitle, cardText, bgClass }) => {
 
     return (
         <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
-            <figure className='pl-5'><img src={img} alt="Album" /></figure>
+            <figure className='pl-5 pt-5'><img src={img} alt="Album" /></figure>
             <div className="card-body text-white">
                 <h2 className="card-title">{cardTitle} </h2>
                 <p>{cardText}</p>
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -12,11 +12,10 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-130px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold py-3'>Appointment</h3>
                 <h2 className='text-3xl text-white pb-3'>Make an Appointment Today</h2>
-                <p className='text-white pb-3'>
-                    TO REQUEST FOR AN ONLINE APPOINTMENT, PLEASE COMPLETE THE FORM BELOW AND CLICK THE “REQUEST” BUTTON.
+                <p className='text-white pb-5'>
                     Your appointment will be confirmed through return e-mail or telephonic communication. Please be informed that this submission of “Request for Appointment” will only be workable after confirmation by our Appointment Centre. Confirmation of an appointment depends upon the availability of doctors at your preferred date and time.
                     TO MAKE AN APPOINTMENT VIA PHONE, OR FOR FURTHER INFORMATION REGARDING YOUR APPOINTMENT, PLEASE CALL OUR APPOINTMENT CENTRE AT THE FOLLOWING NUMBERS Phone: 01719555207 , 01533654259</p>
                 <Button>Get Started</Button>
