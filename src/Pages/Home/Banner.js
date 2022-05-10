d9e6c48f0dba3fefd8e5cb42f45755627008eaac
noormohammad20 <mdsaim204@gmail.com>
2022-05-10 23:42:54 +0600

    add footer component add solvedsome warning

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,12 +3,12 @@ import chair from '../../assets/images/chair.png'
 import Button from '../Shared/Button'
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-[url('/src/assets/images/bg.png')] bg-cover">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='' />
+        <div className="hero min-h-screen bg-[url('/src/assets/images/bg.png')] bg-cover">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
-                    <h1 class="text-7xl font-bold">Your New Smile Starts Hare</h1>
-                    <p class="py-6">Specially designed for patients seeking dentistry abroad, we offer you bespoke expertise at a price that is unbelievably affordable. We have an elite team of carefully selected specialists from all fields of dentistry.</p>
+                    <h1 className="text-7xl font-bold">Your New Smile Starts Hare</h1>
+                    <p className="py-6">Specially designed for patients seeking dentistry abroad, we offer you bespoke expertise at a price that is unbelievably affordable. We have an elite team of carefully selected specialists from all fields of dentistry.</p>
                     <Button>Get Started</Button>
                 </div>
             </div>
