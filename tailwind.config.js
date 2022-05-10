e60e6e833845743bd986cbe6653f840e8c30aa33
noormohammad20 <mdsaim204@gmail.com>
2022-05-10 22:33:27 +0600

    add contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ module.exports = {
           primary: "#0FCFEC",
           secondary: "#19D3AE",
           accent: "#3A4256",
-          neutral: "#3d4451",
           "base-100": "#ffffff",
         },
       },
