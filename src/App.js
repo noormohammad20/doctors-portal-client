facf0208dee5b1a1b9a5b47067c97c1317dc90bf
noormohammad20 <mdsaim204@gmail.com>
2022-05-11 23:45:02 +0600

    Implement Registration Form to create new user

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About/About'
 import Appointment from './Pages/Appointment/Appointment'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
+import Signup from './Pages/Login/Signup'
 import Navbar from './Pages/Shared/Navbar'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='about' element={<About></About>}></Route>
         <Route path='appointment' element={<Appointment></Appointment>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
+        <Route path='signup' element={<Signup></Signup>}></Route>
       </Routes>
     </div>
   )
--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init'
 import { useForm } from "react-hook-form"
 import Loading from '../Shared/Loading'
 import { Link } from 'react-router-dom'
-const Login = () => {
+const Signup = () => {
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth)
     const { register, formState: { errors }, handleSubmit } = useForm()
     const [
-        signInWithEmailAndPassword,
+        createUserWithEmailAndPassword,
         user,
         loading,
         error,
-    ] = useSignInWithEmailAndPassword(auth)
-
+    ] = useCreateUserWithEmailAndPassword(auth)
     let signInError
 
     if (error || googleError) {
@@ -30,17 +29,37 @@ const Login = () => {
 
     const onSubmit = data => {
         console.log(data)
-        signInWithEmailAndPassword(data.email, data.password)
+        createUserWithEmailAndPassword(data.email, data.password)
 
     }
     return (
+
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <h2 className="text-center text-2xl font-bold">Sign Up</h2>
 
                     <form onSubmit={handleSubmit(onSubmit)}>
 
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text" placeholder="Enter Your Name"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("name", {
+                                    required: {
+                                        value: true,
+                                        message: 'Name Is Required!'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                {errors.name?.type === 'required' && <span class="label-text-alt text-red-500">{errors.name.message}</span>}
+
+                            </label>
+                        </div>
                         <div class="form-control w-full max-w-xs">
                             <label class="label">
                                 <span class="label-text">Email</span>
@@ -88,9 +107,9 @@ const Login = () => {
                             </label>
                         </div>
                         {signInError}
-                        <input className='btn w-full max-w-xs' type="submit" value="Login" />
+                        <input className='btn w-full max-w-xs' type="submit" value="Sign Up" />
                     </form>
-                    <p><small>New to Doctors Portal? <Link className='text-primary' to='/signup'>Create New Account</Link> </small></p>
+                    <p><small>Already have an account?  <Link className='ml-2 text-primary' to='/login'>Please Login</Link> </small></p>
 
                     <div className="divider">OR</div>
                     <button
@@ -100,8 +119,7 @@ const Login = () => {
                 </div>
             </div>
         </div>
-
     )
 }
 
-export default Login
+export default Signup
