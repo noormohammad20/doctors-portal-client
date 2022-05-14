2a112c5e405269414c049bd5b3801e8274ffb245
noormohammad20 <mdsaim204@gmail.com>
2022-05-14 14:35:46 +0600

    Display User specific Appointments using Data Table

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../firebase.init'
+
 
 const MyAppointment = () => {
+    const [appointments, setAppointment] = useState([])
+    const [user] = useAuthState(auth)
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointment(data))
+        }
+    }, [user])
+
     return (
         <div>
-            <h2>My Appointment</h2>
+            <h2>My Appointment: {appointments.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appointments.map(appointment => <tr>
+                                <th>1</th>
+                                <th>{appointment.patientName}</th>
+                                <td>{appointment.date}</td>
+                                <td>{appointment.slot}</td>
+                                <td>{appointment.treatment}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
