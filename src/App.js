625126b51c2aa32e24a1d01d8dbfbac2023a89ab
noormohammad20 <mdsaim204@gmail.com>
2022-05-14 11:34:06 +0600

    Create Dashboard route with Drawer and Nested Route

--- src/App.js
+++ src/App.js
@@ -9,21 +9,31 @@ import Signup from './Pages/Login/Signup'
 import Navbar from './Pages/Shared/Navbar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import MyAppointment from './Pages/Dashboard/MyAppointment'
+import MyReview from './Pages/Dashboard/MyReview'
 
 function App() {
   return (
     <div className='max-w-7xl mx-auto px-12'>
-      <Navbar></Navbar>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='home' element={<Home></Home>}></Route>
-        <Route path='about' element={<About></About>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='home' element={<Home />}></Route>
+        <Route path='about' element={<About />}></Route>
         <Route path='appointment' element={
           <RequireAuth>
-            <Appointment></Appointment>
+            <Appointment />
           </RequireAuth>}></Route>
-        <Route path='login' element={<Login></Login>}></Route>
-        <Route path='signup' element={<Signup></Signup>}></Route>
+        <Route path='dashboard' element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>}>
+          <Route index element={<MyAppointment></MyAppointment>}></Route>
+          <Route path='review' element={<MyReview></MyReview>}></Route>
+        </Route>
+        <Route path='login' element={<Login />}></Route>
+        <Route path='signup' element={<Signup />}></Route>
       </Routes>
       <ToastContainer />
     </div>
---
+++ src/Pages/Dashboard/MyReview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyReview = () => {
+    return (
+        <div>
+            <h2>Reviews</h2>
+        </div>
+    )
+}
+
+export default MyReview
