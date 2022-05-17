c57382c07b1482032fcb49db47925a4496fc243b
noormohammad20 <mdsaim204@gmail.com>
2022-05-18 01:40:12 +0600

    Server side payment intents API to get client Secret from stripe

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     CardElement,
     useStripe,
     useElements,
 } from '@stripe/react-stripe-js'
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ appointment }) => {
     const stripe = useStripe()
     const elements = useElements()
     const [cardError, setCardError] = useState('')
+    const [clientSecret, setClientSecret] = useState('')
+
+    const { price } = appointment
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret)
+                }
+            })
+    }, [price])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -51,7 +70,7 @@ const CheckoutForm = () => {
                         },
                     }}
                 />
-                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
---
+++ src/Pages/Dashboard/__payment_steps.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * 1. install stripe react stripe js
+ * 2. open stripe account on stripe website
+ * 3. get publishable key ____pk
+ * 4. Creates Elements wrapper using publishable key
+ * 5. create checkout form using card element and use useStripe(),useElements()
+ * 6. get card element info(credit card info)
+ * 7. get credit card info/error and display card error (if any)
+ *  ----------------------------------
+ * 
+*/
