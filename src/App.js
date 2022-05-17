ceb4885c15b0c6c3ee0329632d15aa0b296c0602
noormohammad20 <mdsaim204@gmail.com>
2022-05-17 11:56:19 +0600

    VerifyAdmin middleware and load all doctors

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import MyHistory from './Pages/Dashboard/MyHistory'
 import Users from './Pages/Dashboard/Users'
 import RequireAdmin from './Pages/Login/RequireAdmin'
 import AddDoctor from './Pages/Dashboard/AddDoctor'
+import ManageDoctors from './Pages/Dashboard/ManageDoctors'
 
 function App() {
   return (
@@ -43,6 +44,10 @@ function App() {
           <Route path='addDoctor' element={
             <RequireAdmin>
               <AddDoctor />
+            </RequireAdmin>} />
+          <Route path='manageDoctor' element={
+            <RequireAdmin>
+              <ManageDoctors />
             </RequireAdmin>} />
         </Route>
         <Route path='login' element={<Login />} />
