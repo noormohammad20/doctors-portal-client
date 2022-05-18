d66152c78b2b658439d191dfb043c896f4e7df68
noormohammad20 <mdsaim204@gmail.com>
2022-05-18 12:25:29 +0600

    Process card confirmation for Payment and display success Message

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -9,9 +9,11 @@ const CheckoutForm = ({ appointment }) => {
     const stripe = useStripe()
     const elements = useElements()
     const [cardError, setCardError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [transactionId, setTransactionId] = useState('')
     const [clientSecret, setClientSecret] = useState('')
 
-    const { price } = appointment
+    const { price, patient, patientName } = appointment
     useEffect(() => {
         fetch('http://localhost:5000/create-payment-intent', {
             method: 'POST',
@@ -47,6 +49,30 @@ const CheckoutForm = ({ appointment }) => {
         })
 
         setCardError(error?.message || '')
+        setSuccess('')
+
+        // confirm card payment
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: patient,
+                    },
+                },
+            },
+        )
+        if (intentError) {
+            setCardError(intentError?.message)
+        }
+        else {
+            setCardError('')
+            setTransactionId(paymentIntent.id)
+            console.log(paymentIntent)
+            setSuccess('Congrats! Your Payment Is Completed')
+        }
 
     }
 
@@ -75,6 +101,10 @@ const CheckoutForm = ({ appointment }) => {
                 </button>
             </form>
             {cardError && <p className='text-red-500'>{cardError}</p>}
+            {success && <div className='text-green-500'>
+                <p>{success}</p>
+                <p>Your Transaction Id:<span className="text-orange-500 font-bold">{transactionId}</span></p>
+            </div>}
         </>
     )
 }
